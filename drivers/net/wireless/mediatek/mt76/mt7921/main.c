28ca0a3c03d0658eb7a490a1d025a3db64e7ebeb
Zac Bowling <zac@zacbowling.com>
2026-01-14 19:58:05 -0800

    wifi: mt76: mt7921: fix missing mutex protection in multiple paths
    Add mt792x_mutex_acquire/release around ieee80211_iterate_active_interfaces()
    and ieee80211_iterate_interfaces() calls in mt7921 driver to prevent race
    conditions:
    
    - mt7921_roc_abort_sync(): protect ROC abort iteration
    - mt7921_set_runtime_pm(): protect runtime PM iteration
    - mt7921_mac_reset_work(): protect vif reconnect iteration after reset
    
    These paths were missing the mutex protection that is required when
    calling ieee80211_iterate_* functions with ITER_RESUME_ALL flag.
    
    Signed-off-by: Zac Bowling <zac@zacbowling.com>

--- drivers/net/wireless/mediatek/mt76/mt7921/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/main.c
@@ -373,10 +373,12 @@ void mt7921_roc_abort_sync(struct mt792x_dev *dev)
 
 	timer_delete_sync(&phy->roc_timer);
 	cancel_work_sync(&phy->roc_work);
+	mt792x_mutex_acquire(dev);
 	if (test_and_clear_bit(MT76_STATE_ROC, &phy->mt76->state))
 		ieee80211_iterate_interfaces(mt76_hw(dev),
 					     IEEE80211_IFACE_ITER_RESUME_ALL,
 					     mt7921_roc_iter, (void *)phy);
+	mt792x_mutex_release(dev);
 }
 EXPORT_SYMBOL_GPL(mt7921_roc_abort_sync);
 
@@ -619,9 +621,11 @@ void mt7921_set_runtime_pm(struct mt792x_dev *dev)
 	bool monitor = !!(hw->conf.flags & IEEE80211_CONF_MONITOR);
 
 	pm->enable = pm->enable_user && !monitor;
+	mt792x_mutex_acquire(dev);
 	ieee80211_iterate_active_interfaces(hw,
 					    IEEE80211_IFACE_ITER_RESUME_ALL,
 					    mt7921_pm_interface_iter, dev);
+	mt792x_mutex_release(dev);
 	pm->ds_enable = pm->ds_enable_user && !monitor;
 	mt76_connac_mcu_set_deep_sleep(&dev->mt76, pm->ds_enable);
 }
